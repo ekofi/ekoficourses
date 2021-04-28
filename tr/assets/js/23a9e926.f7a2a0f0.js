802dda648edbb74074975d6041bf2c851113b738
Ferhat Suat Erdogan <ferhat@ekofi.science>
2021-04-28 20:42:21 +0300

    lolo

---
+++ tr/assets/js/23a9e926.f7a2a0f0.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{80:function(o){o.exports=JSON.parse('{"allTagsPath":"/tr/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/tr/blog/tags/facebook"}')}}]);

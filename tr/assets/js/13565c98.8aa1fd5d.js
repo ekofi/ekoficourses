e619035b4d70edf2f936960e832023c5b6c30c4b
Ferhat Suat Erdogan <ferhat@ekofi.science>
2021-05-09 18:57:41 +0300

    Ded

---
+++ tr/assets/js/13565c98.8aa1fd5d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{74:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/tr/blog/welcome"},{"title":"Hello","permalink":"/tr/blog/hello-world"},{"title":"Hola","permalink":"/tr/blog/hola"}]}')}}]);
